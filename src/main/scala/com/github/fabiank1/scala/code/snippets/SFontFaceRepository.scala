587dc23e1d509c9ade7fe6cd7efc82a6e67b06f2
FabianK1 <38921138+FabianK1@users.noreply.github.com>
2021-09-23 11:00:25 +0200

    Create SFontFaceRepository.scala

---
+++ src/main/scala/com/github/fabiank1/scala/code/snippets/SFontFaceRepository.scala
@@ -0,0 +1,51 @@
+class SFontFaceRepository {
+
+  val LATIN_MODERN = {
+    """
+    @font-face {
+        font-family: 'Latin Modern Sans';
+        src: url('https://raw.githubusercontent.com/slashfoo/lmweb/master/font/lmsans-boldoblique-webfont.eot');
+        src: url('https://raw.githubusercontent.com/slashfoo/lmweb/master/font/lmsans-boldoblique-webfont.eot?#iefix') format('embedded-opentype'),
+             url('https://raw.githubusercontent.com/slashfoo/lmweb/master/font/lmsans-boldoblique-webfont.woff') format('woff'),
+             url('https://raw.githubusercontent.com/slashfoo/lmweb/master/font/lmsans-boldoblique-webfont.ttf') format('truetype'),
+             url('https://raw.githubusercontent.com/slashfoo/lmweb/master/font/lmsans-boldoblique-webfont.svg#latin_modern_sans10BdOb') format('svg');
+        font-weight: bold;
+        font-style: oblique;
+    }
+    
+    @font-face {
+        font-family: 'Latin Modern Sans';
+        src: url('https://raw.githubusercontent.com/slashfoo/lmweb/master/font/lmsans-bold-webfont.eot');
+        src: url('https://raw.githubusercontent.com/slashfoo/lmweb/master/font/lmsans-bold-webfont.eot?#iefix') format('embedded-opentype'),
+             url('https://raw.githubusercontent.com/slashfoo/lmweb/master/font/lmsans-bold-webfont.woff') format('woff'),
+             url('https://raw.githubusercontent.com/slashfoo/lmweb/master/font/lmsans-bold-webfont.ttf') format('truetype'),
+             url('https://raw.githubusercontent.com/slashfoo/lmweb/master/font/lmsans-bold-webfont.svg#latin_modern_sans10_bold') format('svg');
+        font-weight: bold;
+        font-style: normal;
+    }
+    
+    @font-face {
+        font-family: 'Latin Modern Sans';
+        src: url('https://raw.githubusercontent.com/slashfoo/lmweb/master/font/lmsans-oblique-webfont.eot');
+        src: url('https://raw.githubusercontent.com/slashfoo/lmweb/master/font/lmsans-oblique-webfont.eot?#iefix') format('embedded-opentype'),
+             url('https://raw.githubusercontent.com/slashfoo/lmweb/master/font/lmsans-oblique-webfont.woff') format('woff'),
+             url('https://raw.githubusercontent.com/slashfoo/lmweb/master/font/lmsans-oblique-webfont.ttf') format('truetype'),
+             url('https://raw.githubusercontent.com/slashfoo/lmweb/master/font/lmsans-oblique-webfont.svg#latin_modern_sans10_oblique') format('svg');
+        font-weight: normal;
+        font-style: oblique;
+    }
+    
+    @font-face {
+        font-family: 'Latin Modern Sans';
+        src: url('https://raw.githubusercontent.com/slashfoo/lmweb/master/font/lmsans-regular-webfont.eot');
+        src: url('https://raw.githubusercontent.com/slashfoo/lmweb/master/font/lmsans-regular-webfont.eot?#iefix') format('embedded-opentype'),
+             url('https://raw.githubusercontent.com/slashfoo/lmweb/master/font/lmsans-regular-webfont.woff') format('woff'),
+             url('https://raw.githubusercontent.com/slashfoo/lmweb/master/font/lmsans-regular-webfont.ttf') format('truetype'),
+             url('https://raw.githubusercontent.com/slashfoo/lmweb/master/font/lmsans-regular-webfont.svg#latin_modern_sans10_regular') format('svg');
+        font-weight: normal;
+        font-style: normal;
+    }
+    """
+  }
+
+}
